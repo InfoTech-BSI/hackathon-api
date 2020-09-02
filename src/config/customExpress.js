4c5bc6b3ccaeaaf4c250275b42e89719ad047ee2
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-01 21:05:53 -0300

    Adicionando cors

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -34,6 +34,14 @@ module.exports = () => {
     app.use('/sinais', sinaisRoute);
     app.use('/unidadeSaude', unidadeSaudeRoute);
 
+    //Habilita o CORS
+    app.use(function(req, res, next){
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+        res.header('Access-Control-Allow-Headers', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    });
+
     //carregar automaticamente os controllers
     // consign() 
     //     .include('controllers')
