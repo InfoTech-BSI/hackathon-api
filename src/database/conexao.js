07ffbb913102ae0da24e252114b87d63196b134f
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-04 18:22:11 -0300

    Alteração na conexão do banco - amazon

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -3,11 +3,13 @@
 const mysql = require('mysql');
 
 const conexao  = mysql.createConnection({
-    host: 'localhost',
+    host: 'database-1.ckttgzjulq7d.us-east-1.rds.amazonaws.com', //'localhost'
     port: '3306',
-    user: 'root',
-    password: '',
-    database: 'covid'
+    user: 'admin', //root
+    password: 'info_bsi', //''
+    //database: 'covid'
 });
+
+IP: 
 
 module.exports = conexao;
