38d184be9946ae371b7c5bced0a826de0c100068
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-04 21:41:26 -0300

    cors - 2

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -22,12 +22,12 @@ module.exports = () => {
 
     //Habilita o CORS
     app.use(cors());
-    // app.use(function(req, res, next){
-    //     res.header('Access-Control-Allow-Origin', "*");
-    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    //     res.header('Access-Control-Allow-Headers', 'GET, POST, PUT, DELETE, OPTIONS');
-    //     next();
-    // });
+    app.use(function(req, res, next){
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+        res.header('Access-Control-Allow-Headers', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    });
     
     app.use(bodyParser.urlencoded({ 
         extended: false
