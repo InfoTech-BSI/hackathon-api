51103047d141477ece9cf93f1424e2e8023eaca0
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-04 21:45:14 -0300

    Alteração no cors

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -22,9 +22,10 @@ module.exports = () => {
 
     //Habilita o CORS
     app.use(cors());
+    app.options('*', cors());
     app.use(function(req, res, next){
         res.header('Access-Control-Allow-Origin', "*");
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.header('Access-Control-Allow-Headers', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
     });
